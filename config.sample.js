491cc3212c4fe4c9143fb1dd832dae11aa9edf9e
王宇宁 <wangyuning@wangyuningdeMacBook-Pro.local>
2019-08-13 22:21:09 +0800

    add file

---
+++ config.sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  github: {
+    request_token_url: 'https://github.com/login/oauth/access_token',
+    client_id: '',
+    client_secret: '',
+  },
+}
+
+// token 8de99c7accd48c7476c264e6a1971b38f26183fc
